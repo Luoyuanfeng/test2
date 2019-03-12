deadfa7876dec2ca751e1f2d9880605d417a539c
luoyf <lyf8070@gmail.com>
2019-03-13 01:24:15 +0800

    modify

--- pages/active.jsx
+++ pages/active.jsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import Layout from "./layout"
 
+const placeholder = {
+    width: 200,
+    height: 200,
+    backgroundColor: "#aaaaaa"
+}
+
 export default class Active extends Component {
+
+    componentWillMount() {
+        console.log('aaaaaaaa')
+    }
+
     render() {
-        return (
-            <Layout>
-               <p>This is active page</p>
-            </Layout>
-        )
+        if (1) {
+            return (
+                <Layout>
+                   <p>This is active page</p>
+                </Layout>
+            )
+        } else {
+            return (
+                <div style={placeholder}></div>
+            )
+        }
     }
 }
